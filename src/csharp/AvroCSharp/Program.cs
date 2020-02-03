a4bdbbcb903690921c04ecc8dbdb150a6e76cde1
Martin Jul <no-spam@mjul.com>
2020-02-03 11:31:59 +0100

    Add skeleton F# and C# projects

---
+++ src/csharp/AvroCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AvroCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
